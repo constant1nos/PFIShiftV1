194251fb9df2e20664302c075a7aab6fd4b6b936
dinob <dinobatzio@gmail.com>
2017-11-19 11:12:04 +0200

    Merge branch 'master' of https://github.com/constant1nos/PFIShiftV1
    # Conflicts:
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/example/dinob/pfishiftv1/appPreferences.java
@@ -0,0 +1,59 @@
+package com.example.dinob.pfishiftv1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * This is a Singleton Class to manage and maintain data
+ * stored in shared preferences XML.
+ * Very handy for other future apps
+ * Created by dbatz 17/11/2017
+ */
+
+public class appPreferences {
+
+    private final SharedPreferences sharedPref;
+    private SharedPreferences.Editor sharedPrefEditor;
+    private static appPreferences instance;
+
+    // Constructor
+    private appPreferences(Context context) {
+        sharedPref = context.getSharedPreferences(context.getString(R.string.shared_preferences_file), Context.MODE_PRIVATE);
+        sharedPrefEditor = sharedPref.edit();
+    }
+
+    // Method to hold only one instance
+    public static appPreferences getInstance(Context context) {
+        if (instance == null) {
+            instance = new appPreferences(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    public void savePref(String key, int var){
+        sharedPrefEditor.putInt(key, var);
+        sharedPrefEditor.apply();
+    }
+
+    public void savePref(String key, String var) {
+        sharedPrefEditor.putString(key, var);
+        sharedPrefEditor.apply();
+    }
+
+    public void savePref(String key, boolean var) {
+        sharedPrefEditor.putBoolean(key, var);
+        sharedPrefEditor.apply();
+    }
+
+    public int getPref(String key, int var) {
+        return sharedPref.getInt(key, var);
+    }
+
+    public String getPref(String key, String var) {
+        return sharedPref.getString(key, var);
+    }
+
+    public Boolean getPref(String key, Boolean var) {
+        return sharedPref.getBoolean(key, var);
+    }
+}
